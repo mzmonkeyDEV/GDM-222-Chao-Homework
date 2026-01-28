c2ebbbc0594df7b451e60701727518cdb68e6f5c
mzmonkeyDEV <mzmonkey001@gmail.com>
2026-01-28 11:58:07 +0700

    itemUsage

--- Assets/Workshop/Solutions/Scripts/Week03/Item/ItemKey.cs
+++ Assets/Workshop/Solutions/Scripts/Week03/Item/ItemKey.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Solution
 {
+    [CreateAssetMenu(fileName = "ItemPotion",menuName = "Item/ItemKey")]
     public class ItemKey : MonoBehaviour
     {
         
--- Assets/Workshop/Solutions/Scripts/Week03/OOPItem.cs
+++ Assets/Workshop/Solutions/Scripts/Week03/OOPItem.cs
@@ -5,12 +5,14 @@ namespace Solution
 
     public class OOPItem : Identity
     {
+        public ItemData ItemData;
         public override void Hit(Identity hitBy)
         {
             base.Hit(hitBy);
             if (hitBy is Character)
             {
                 mapGenerator.UpdatePositionIdentity(hitBy,positionX,positionY);
+                ItemData.Use(hitBy);
                 Destroy(gameObject);
             }
         }
